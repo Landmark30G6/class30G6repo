1f89ced3d36297fd8d42645834521d94b286501d
root <root@ip-10-0-1-202.ec2.internal>
2022-11-21 20:43:25 +0000

    small changes

--- anothertest.sh
+++ anothertest.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #the script will be used to check system resources 
+cd /tmp
 sudo ps -ef > process.log
 sudo df -h >>process.log
 sudo free -m >> process.log
 sudo w >> process.log
-sudo gzip process.log/tmp
+sudo gzip process.log
 sudo echo "Author: Elvis B"
 
