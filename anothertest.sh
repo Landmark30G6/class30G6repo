637d478a0325ffb9f725197d923f964213b0cdc5
root <root@ip-10-0-1-202.ec2.internal>
2022-11-21 20:12:46 +0000

    anothertest job

---
+++ anothertest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#the script will be used to check system resources 
+ps -ef > process.log
+df -h >>process.log
+free -m >> process.log
+w >> process.log
+gzip process.log/tmp
+echo complete
+
