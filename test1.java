8477620e907ba5d80fed6837f151c8fee8ab1836
Samueto Tolambo <stongambo@gmail.com>
2022-11-22 16:25:05 +0000

    My first commit

---
+++ test1.java
@@ -0,0 +1,3 @@
+ 16:11:14 up 20:27,  1 user,  load average: 0.00, 0.00, 0.00
+USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
+simon    pts/0    pool-96-241-155- 15:39    0.00s  0.06s  0.01s sshd: simon [priv]  
---
+++ test2.java
@@ -0,0 +1,7 @@
+Filesystem     1K-blocks    Used Available Use% Mounted on
+devtmpfs          485312       0    485312   0% /dev
+tmpfs             494456       0    494456   0% /dev/shm
+tmpfs             494456     436    494020   1% /run
+tmpfs             494456       0    494456   0% /sys/fs/cgroup
+/dev/xvda1       8376300 1856852   6519448  23% /
+tmpfs              98892       0     98892   0% /run/user/1002
