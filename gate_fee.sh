d6e3bc124b39aca6a6396851a8ec23044b4d5e5b
elvis.ngeh <evis.bantar@gmail.com>
2022-11-22 13:14:05 -0500

    added extra line

---
+++ gate_fee.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#script to admit fans into a soccer international games
+echo  amazing manchester fans are welcome to the cup finals
+echo enter gate fee amount
+read fee
+echo  enter number of fans
+read fans
+amount=`expr $fans \* $fee`
+echo Your group will pay $amount USD
