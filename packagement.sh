e19017d2ef1e93444c94e30f36b81421ab076c1d
elvis.ngeh <evis.bantar@gmail.com>
2022-11-22 15:32:01 -0500

    added some extra lines

--- packagement.sh
+++ packagement.sh
@@ -3,6 +3,13 @@ pkg(){
 echo "what do you want to install ?"
 read command 
 sudo yum -y >> /tmp/yum.log
+sudo cat /tmp/yum.log | grep "Nothing to do"
+message="Nothing to do"
+if [ $message -eq "Nothing to do" ] 
+then
+echo "pkg is already installed "
+else "continue with the installation"
+sudo yum -y >> /tmp/yum.log
 cd /tmp
 sudo gzip yum.log
 }
