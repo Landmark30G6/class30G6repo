6f64f6c0d10b101405759cadd850772bb9157b69
elvis.ngeh <evis.bantar@gmail.com>
2022-11-21 23:39:58 -0500

    useradd function

---
+++ function.sh
@@ -0,0 +1,21 @@
+usermgt(){
+  #!/bin/bash
+  #script is build to create a user,assign user to a group...etc
+  echo username
+  read username
+  sudo useradd $username
+  echo password
+  read -s password
+  sudo passwd $username
+  cat /etc/passwd | grep $username
+  echo congratulations your login name is $username
+  # the next part is to create a group and assisgn user to it 
+  echo your group name
+  read group
+  sudo groupadd $group
+  echo add $username to group
+  sudo usermod -aG $group $username
+  echo $?
+  echo display $username
+  sudo lid $username
+}
