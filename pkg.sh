b3ddc9b37dbc84a1d0eab70526a5c104673fb3e4
elvis.ngeh <evis.bantar@gmail.com>
2022-11-22 15:52:34 -0500

    added an extra line

--- pkg.sh
+++ pkg.sh
@@ -6,9 +6,9 @@ read yum
 yum install -y $install
 sudo yum -y >> /tmp/yum.log
 sudo cat /tmp/yum.log | grep "Nothing to do"
-message="Nothing to do"
 if [ $message -eq "Nothing to do" ] 
 then
+message="Nothing to do"
 echo "pkg is already installed "
 else "continue with the installation"
 fi
