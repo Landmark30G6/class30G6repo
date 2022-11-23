b95326a97c810952f9a50a665ee89926d3683650
elvis.ngeh <evis.bantar@gmail.com>
2022-11-23 00:17:51 -0500

    switch_0

---
+++ shedule.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "what are your activities this week"
+case $1 in
+     monday)
+           echo "I will be doing DevOps review ";;
+     Tuesday)
+           echo "I will be leading devOps team meeting";;
+     Wednesday)
+           echo "I have bible study from 8  - 9 PM EST";;
+     Thursday)
+           echo "will be taking the kids for swim school makeup class";;
+     Friday)
+           echo "i will just relax and watch movies";;
+      *)
+           echo "enter an schedule request for the days of the week";;
