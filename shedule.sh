7f46f7839a8fe416ff9749f4b0eb463de88eebe1
elvis.ngeh <evis.bantar@gmail.com>
2022-11-23 00:19:59 -0500

    added extra lines

--- shedule.sh
+++ shedule.sh
@@ -2,14 +2,14 @@
 echo "what are your activities this week"
 case $1 in
      monday)
-           echo "I will be doing DevOps review ";;
+           echo "I will be doing DevOps review " ;;
      Tuesday)
-           echo "I will be leading devOps team meeting";;
+           echo "I will be leading devOps team meeting" ;;
      Wednesday)
-           echo "I have bible study from 8  - 9 PM EST";;
+           echo "I have bible study from 8  - 9 PM EST" ;;
      Thursday)
-           echo "will be taking the kids for swim school makeup class";;
+           echo "will be taking the kids for swim school makeup class" ;;
      Friday)
-           echo "i will just relax and watch movies";;
+           echo "i will just relax and watch movies" ;;
       *)
            echo "enter an schedule request for the days of the week";;
