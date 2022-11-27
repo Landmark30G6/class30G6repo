a21d9f3728f9c702611f0ec36a961f130c0feb34
Bimpsycutie <118792008+Bimpsycutie@users.noreply.github.com>
2022-11-27 19:26:04 -0400

    car servicing feature added

--- date.sh
+++ date.sh
@@ -2,3 +2,5 @@
 DATE=$(date +%F)
 echo "Good morning ,what is today date ? today's date is $DATE"
 Bugs removed sorry this is a test 
+print (car servives automated)
+print (your car needs servicing)
